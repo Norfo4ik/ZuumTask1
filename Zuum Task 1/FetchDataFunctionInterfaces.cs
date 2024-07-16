7a7c8b134f8d22f41bf2b19ee7a9b627005baad4
Artem Konovchenko <norfo4ik@gmail.com>
2024-07-16 22:47:23 +0300

    Created interfaces for FetchDataFunction

--- Zuum Task 1/FetchDataFunctionInterfaces.cs
+++ Zuum Task 1/FetchDataFunctionInterfaces.cs
@@ -8,17 +8,36 @@ namespace Zuum_Task_1
 {
     interface IApiService
     {
+        Task<ApiResponse> FetchDataAsynk();
+    }
+
+    public class ApiResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Payload { get; set; }
+        public string ErrorMessage { get; set; }
 
     }
 
     interface ILoggingService
     {
+        Task LogSuccessAsync(ApiResponse response);
+        Task LogFailureAsynk(string errorMessage);
+        Task <IEnumerable<LogEntry>> GetLogsAsync(string from,  string to);
+    }
 
+    public class LogEntry
+    {
+        public string Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
     }
 
     interface IBlobStorageService
     {
-
+        Task StorePayloadAsync(string payload);
+        Task<string> GetPayloadAsync(string logId);
     }
 
 
